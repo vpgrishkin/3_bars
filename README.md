1ee8fb7815d7f8e88a53c22c1a35c6969ea01e79
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-02-25 11:47:32 +0300

    done

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
-# Ближайшие бары
+# Ближайшие бары. Задача #3
 
-[FIXME. Здесь будет описание проекта]
+На сайте data.mos.ru есть много разных данных, в том числе список московских баров (https://data.mos.ru/opendata/7710881420-bary). Его можно скачать в формате json.
+
+Требуется написать скрипт, который рассчитает:
+самый большой бар;
+самый маленький бар;
+самый близкий бар (текущие gps-координаты ввести с клавиатуры).
 
 # Как запустить
 
-Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5, default.json, который можно скачать с https://data.mos.ru/opendata/7710881420-bary. Можно указать другое имя файла, указав его при запуске.
 
 Запуск на Linux:
 
 ```#!bash
 
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+$ python3 bars.py # possibly requires call of python3 executive instead of just python
+# Загружен файл default.json
+Введите коодинаты longitude:1
+Введите коодинаты latitude:1
+Ближащий объект
+{'Name': 'Staropramen', 'AdmArea': 'Центральный административный округ', 'IsNetObject': 'нет', 'Address': 'Садовая-Спасская улица, дом 19, корпус 1', 'ID': '00146638', 'Latitude_WGS84': '55.3033000000000000', 'global_id': 281494712, 'PublicPhone': [{'global_id': 34992.0, 'global_object_id': 281494712.0, 'system_object_id': '00146638 _1', 'PublicPhone': '(985) 069-34-47'}], 'SeatsCount': 50, 'TypeObject': 'бар', 'geoData': {'type': 'Point', 'coordinates': [36.900000000253, 55.303299999814]}, 'system_object_id': '00146638', 'Longitude_WGS84': '36.9000000000000000', 'District': 'Красносельский район', 'SocialPrivileges': 'нет'}
+
 
 ```
 
--- bars.py
+++ bars.py
@@ -1,21 +1,68 @@
-import json
+DEFAULT_FILE_NAME = 'default.json'
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+	import json
+
+
+	dict_from_json = None
+	
+	try:
+		file = open(file_path, 'r', encoding='windows-1251')
+	except Exception:
+		print(u'Не удалось открыть файл')
+		sys.exit(1)
+
+	try:
+		text = file.read()
+	except Exception:
+		print('Ошибка. Не удалось загрузить файл.')
+	else:
+		print('Загружен файл {}'.format(file_path))
+	finally:
+		file.close()
+
+	dict_from_json = json.loads(text)
+	return dict_from_json
 
 
 def get_biggest_bar(data):
-    pass
+	biggest_bar = max(data, key=lambda x: x['SeatsCount'])
+	return biggest_bar
+	
 
 
 def get_smallest_bar(data):
-    pass
+	smallest_bar = min(data, key=lambda x: x['SeatsCount'])
+	return smallest_bar
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
-
+# for perfectionists https://pypi.python.org/pypi/haversine
+	from math import hypot
+	get_closest_bar = min(data, key=lambda x: hypot(float(x['Longitude_WGS84']) - longitude, float(x['Latitude_WGS84']) - latitude))
+	return get_closest_bar
 
 if __name__ == '__main__':
-    pass
+	import sys
+	import os
+	
+	if len(sys.argv) == 1:
+		print('Нет параметров для запуска!  Файл по умолчанию: {}'.format(DEFAULT_FILE_NAME))
+		work_file = DEFAULT_FILE_NAME
+	else:
+		if os.path.isfile(work_file):
+			print('Рабочий файл: ' + work_file)
+			work_file = sys.argv[1]
+
+	
+	data_from_json = load_data(work_file)
+
+	get_biggest_bar(data_from_json)
+	get_smallest_bar(data_from_json)
+	
+	longitude = float(input('Введите longitude:'))
+	latitude = float(input('Введите latitude:'))
+	closest_bar = get_closest_bar(data_from_json, longitude, latitude)
+	print('Ближайший объект')
+	print(closest_bar)
